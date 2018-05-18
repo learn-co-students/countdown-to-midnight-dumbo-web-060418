8dd01a4509376461990c75d67925ab64ce4da840
Jesus Escalona <eyisus@gmail.com>
2018-05-17 23:31:35 -0400

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,12 @@
 #write your code here
+def countdown_with_sleep(k=1)
+  sleep(k)
+end
 
-def countdown
+def countdown(n)
+  until n==0
+    puts "#{n} SECOND(S)!"
+    n-=1
+  end
+  return "HAPPY NEW YEAR!"
 end
